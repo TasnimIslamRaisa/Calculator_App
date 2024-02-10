a78932487cec5677722e3a51eaa042cc276618ed
Tasnim Islam Raisa <islamtasnim65@gmail.com>
2024-02-10 20:59:58 +0600

    updated

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,115 +1,74 @@
 import 'package:calculator/constant/color.dart';
+import 'package:calculator/screen/widgetsButton.dart';
 import 'package:calculator/widgets/Button.dart';
 import 'package:calculator/widgets/textfield.dart';
 import 'package:flutter/material.dart';
 
 class home_screen extends StatelessWidget {
   const home_screen({
-    super.key,
-  });
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final screenHeight = MediaQuery.sizeOf(context).height;
+    final screenHeight = MediaQuery.of(context).size.height;
     final padding = EdgeInsets.symmetric(horizontal: 25, vertical: 35);
     final decoration = BoxDecoration(
-        color: AppColors.primaryColor,
-        borderRadius: BorderRadius.vertical(top: Radius.circular(30)));
+      color: AppColors.primaryColor,
+      borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+    );
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
         title: Center(child: const Text("Calculator App")),
       ),
-      body: Column(children: [
-        CustomeTextField(),
-        Spacer(),
-        Container(
-          height: screenHeight * 0.6,
-          width: double.infinity,
-          padding: padding,
-          decoration: decoration,
-          child: const Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ButtonRow1(
-                    label: "1",
-                    textColor: AppColors.secondaryColor,
+      body: Column(
+        children: [
+          CustomeTextField(),
+          Spacer(),
+          Container(
+            height: screenHeight * 0.6,
+            width: double.infinity,
+            padding: padding,
+            decoration: decoration,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: List.generate(4, (index) => buttonList[index]),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: List.generate(4, (index) => buttonList[index + 4]),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: List.generate(4, (index) => buttonList[index + 8]),
+                ),
+                Row(children: [
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: List.generate(
+                              3, (index) => buttonList[index + 12]),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: List.generate(
+                              3, (index) => buttonList[index + 15]),
+                        ),
+                      ],
+                    ),
                   ),
-                  ButtonRow1(
-                    label: "2",
-                    textColor: AppColors.secondaryColor,
-                  ),
-                  ButtonRow1(
-                    label: "3",
-                    textColor: AppColors.secondaryColor,
-                  ),
-                  ButtonRow1(
-                    label: "4",
-                    textColor: AppColors.secondaryColor,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ButtonRow1(
-                    label: "1",
-                  ),
-                  ButtonRow1(
-                    label: "2",
-                  ),
-                  ButtonRow1(
-                    label: "3",
-                  ),
-                  ButtonRow1(
-                    label: "4",
-                    textColor: AppColors.secondaryColor,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ButtonRow1(
-                    label: "1",
-                  ),
-                  ButtonRow1(
-                    label: "2",
-                  ),
-                  ButtonRow1(
-                    label: "3",
-                  ),
-                  ButtonRow1(
-                    label: "4",
-                    textColor: AppColors.secondaryColor,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ButtonRow1(
-                    label: "1",
-                  ),
-                  ButtonRow1(
-                    label: "2",
-                  ),
-                  ButtonRow1(
-                    label: "3",
-                  ),
-                  ButtonRow1(
-                    label: "4",
-                    textColor: AppColors.secondaryColor,
-                  ),
-                ],
-              ),
-            ],
+                ]),
+              ],
+            ),
           ),
-        ),
-      ]),
-      //drawerEnableOpenDragGesture: false,
+        ],
+      ),
     );
   }
 }
---
+++ lib/screen/widgetsButton.dart
@@ -0,0 +1,66 @@
+import 'package:calculator/constant/color.dart';
+import 'package:calculator/widgets/button.dart';
+import 'package:flutter/material.dart';
+
+List<Widget> buttonList = [
+  const ButtonRow1(
+    label: "C",
+    textColor: AppColors.secondaryColor,
+  ),
+  const ButtonRow1(
+    label: "/",
+    textColor: AppColors.secondaryColor,
+  ),
+  const ButtonRow1(
+    label: "X",
+    textColor: AppColors.secondaryColor,
+  ),
+  const ButtonRow1(
+    label: "AC",
+    textColor: AppColors.secondaryColor,
+  ),
+  const ButtonRow1(
+    label: "7",
+  ),
+  const ButtonRow1(
+    label: "8",
+  ),
+  const ButtonRow1(
+    label: "9",
+  ),
+  const ButtonRow1(
+    label: "+",
+    textColor: AppColors.secondaryColor,
+  ),
+  const ButtonRow1(
+    label: "4",
+  ),
+  const ButtonRow1(
+    label: "5",
+  ),
+  const ButtonRow1(
+    label: "6",
+  ),
+  const ButtonRow1(
+    label: "-",
+    textColor: AppColors.secondaryColor,
+  ),
+  const ButtonRow1(
+    label: "1",
+  ),
+  const ButtonRow1(
+    label: "2",
+  ),
+  const ButtonRow1(
+    label: "3",
+  ),
+  const ButtonRow1(
+    label: "%",
+  ),
+  const ButtonRow1(
+    label: "0",
+  ),
+  const ButtonRow1(
+    label: ".",
+  ),
+];
